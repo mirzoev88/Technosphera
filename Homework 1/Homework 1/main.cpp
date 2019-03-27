85cfbf489e41c9f9aa0d4702e41d57b65d78ec07
Emil <emil.emirzoev@mail.ru>
2019-03-27 16:35:06 +0300

    add gitignore

---
+++ Homework 1/Homework 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework 1
+//
+//  Created by Эмиль Мирзоев on 27/03/2019.
+//  Copyright © 2019 Эмиль Мирзоев. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
